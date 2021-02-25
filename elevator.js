afb5fef68e8c4d5c55c5f78a75433ff262431f0d
Alexis-zn <alexis.zentici@gmail.com>
2021-02-25 17:05:42 +0100

    in progress

---
+++ elevator.js
@@ -0,0 +1,10 @@
+// étages de -2 à 7, deux possibilités: up || down, est-ce qu'il y a déjà 
+// un utilisateur à targetfloor ?
+
+
+let floors= ['-2', '-1', '0', '1', '2', '3', '4', '5', '6', '7'] 
+let currentFloor= '-1'
+let targetFloor= '4'
+let path = currentFloor - targetFloor
+
+
